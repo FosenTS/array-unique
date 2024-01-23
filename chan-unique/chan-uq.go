dab094b797e8e4eb5e70317b5ede567af00fd9b7
Иван Круглов <ivankruglov@MacBook-Air-Ivan.local>
2024-01-23 15:59:36 +0300

    add generics

--- chan-unique/chan-uq.go
+++ chan-unique/chan-uq.go
@@ -4,7 +4,7 @@ import (
 	array_unique "github.com/FosenTS/unique-types/array-unique"
 )
 
-func ConvertToUniqueClosedChan(in chan any) chan any {
+func ConvertToUniqueClosedChan[T any](in chan T) chan T {
 	var a array_unique.UniqueArray
 	var el any
 	var ok bool
@@ -17,7 +17,7 @@ func ConvertToUniqueClosedChan(in chan any) chan any {
 		}
 		a.Add(el)
 	}
-	out := make(chan any)
+	out := make(chan T)
 	for _, el := range a.Elements() {
 		out <- el
 	}
